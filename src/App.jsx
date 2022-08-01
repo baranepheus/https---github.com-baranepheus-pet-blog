4060f1ac7fd04872bb1758e45207edbf858367a7
baranepheus <erika.monjardin@gmail.com>
2022-08-02 01:03:50 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './main-page/Navbar'
+import Main from './main-page/Main'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      < Navbar />
+      < Main />
+    </div>
+  )
+}
+
+export default App
