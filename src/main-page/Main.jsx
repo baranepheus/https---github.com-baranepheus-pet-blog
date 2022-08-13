d923f034537f07468a13415047d10f7396d957a1
baranepheus <erika.monjardin@gmail.com>
2022-08-13 17:01:33 +0800

    navbar

--- src/main-page/Main.jsx
+++ src/main-page/Main.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Main (){
     return(
-        <h1>Navbar goes here</h1>
+        <h1>Main Section goes here</h1>
     )
 }
--- src/main-page/Navbar.jsx
+++ src/main-page/Navbar.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import logo from '../img/logo.png'
 
 export default function Navbar() {
     return(
-        <h1>Navbar goes here</h1>
-    )
+        <div className='nav-container'>
+            <a href="#"><img src={logo} className='logo'/></a>
+            <ul>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>News</a></li>
+                <li><a href='#'>Notes</a></li>
+                <li><a href='#'>About Us</a></li>
+            </ul>
+        </div>
+)
 }
